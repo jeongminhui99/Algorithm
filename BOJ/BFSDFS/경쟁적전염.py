5dc062c19cc47d88655536e43132aedb4876406a
jeongminhui99 <jeongminhee99@naver.com>
2022-01-01 23:44:30 +0900

    Create 경쟁적전염.py

---
+++ BOJ/BFSDFS/경쟁적전염.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+n, k = map(int, input().split())
+
+graph = []
+
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
