3b02e73928d7101631359e7b76a598d6e267a3e6
jeongminhui99 <jeongminhee99@naver.com>
2022-01-13 22:41:12 +0900

    Create 2.py

---
+++ codingtest/n/2.py
@@ -0,0 +1,23 @@
+from extratypes import Tree
+def largestpath(node, m):
+    if (not node):
+        return len(m)
+    if node.x in m:
+        return len(m)
+    else:
+        m[node.x] = 1
+    max_path = max(largestpath(node.l, m), largestpath(node.r, m))
+    m[node.x] -= 1
+
+    if m[node.x] == 0:
+        del m[node.x]
+    return max_path
+
+def solution(T):
+    if(T.l == None and T.r == None):
+        return 1
+    elif T.x == None:
+        return 0
+    else:
+        Hash = {}
+        return largestpath(T, Hash)
