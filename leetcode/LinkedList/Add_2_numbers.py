36b09319bec7c29c26e8d4ed94669dad14a4d57b
jeongminhui99 <jeongminhee99@naver.com>
2022-03-24 00:01:26 +0900

    Update Add_2_numbers.py

--- leetcode/LinkedList/Add_2_numbers.py
+++ leetcode/LinkedList/Add_2_numbers.py
@@ -40,3 +40,29 @@ class Solution:
         resultStr = int(''.join(str(e) for e in a)) + int(''.join(str(e) for e in b))
 
         return self.toReversedLinked(str(resultStr))
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:            
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        root = head = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            sum = 0
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            
+            # 몫(자리올림수)과 나머지(값) 계산
+            # divmod : (a//b, a%b)
+            carry, val = divmod(sum + carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+            
+        return root.next
