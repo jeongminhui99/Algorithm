844486d3b4530a7d0acaff181bea9155ee896310
jeongminhui99 <jeongminhee99@naver.com>
2022-03-24 05:33:28 +0900

    Update Reverse_Linked_List2.py

--- leetcode/LinkedList/Reverse_Linked_List2.py
+++ leetcode/LinkedList/Reverse_Linked_List2.py
@@ -4,23 +4,20 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None:
-            return None
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if not head or left==right:
+            return head
         
-        odd = head
-        even = head.next
-        even_head = head.next #마지막 홀수 짝수 연결을 위한 포인터
+        root = start = ListNode(0)
+        root.next = head
         
-        while even and even.next:
-            # odd.next, odd = odd.next.next, odd.next
-            # 이런 형태는 안됨 odd가 3이 아니라 2가 된다.
-            odd.next = odd.next.next
-            odd = odd.next
-            even.next = even.next.next
-            even = even.next
-            
-        odd.next = even_head
-        return head
-    
-    #odd. even, even_head 변수들은 n의 크기에 상관없이 일정하게 유지 -> 공간복잡도 O(1)
+        for _ in range(left-1):
+            start = start.next
+        end = start.next
+        
+        #반복하면서 노드 차례대로 뒤집기
+        for _ in range(right-left):
+            tmp, start.next, end.next = start.next, end.next, end.next.next
+            start.next.next = tmp
+        
+        return root.next
