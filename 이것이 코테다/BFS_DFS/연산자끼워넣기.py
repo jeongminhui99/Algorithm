b67f0fdbae660004181f8df2f4e1528542a32493
jeongminhui99 <jeongminhee99@naver.com>
2022-05-31 20:48:47 +0900

    Update 연산자끼워넣기.py

--- 이것이 코테다/BFS_DFS/연산자끼워넣기.py
+++ 이것이 코테다/BFS_DFS/연산자끼워넣기.py
@@ -28,12 +28,15 @@ def dfs(i, now):
         if sub > 0:
             sub -= 1
             dfs(i+1, now-data[i])
+            sub += 1
         if mul > 0:
             mul -= 1    
             dfs(i+1, now*data[i])
+            mul += 1
         if div > 0:
             div -= 1
             dfs(i+1, int(now/data[i]))
+            div += 1
 
 dfs(1, data[0])
 print(max_value)
